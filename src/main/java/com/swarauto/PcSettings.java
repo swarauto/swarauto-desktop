adef0b32aa99169678a85b544b5d60b62781c4e8
swarauto <swarauto.help@gmail.com>
2018-05-25 09:48:35 +0700

    1.0.1 update base lib

--- src/main/java/com/swarauto/PcSettings.java
+++ src/main/java/com/swarauto/PcSettings.java
@@ -2,7 +2,7 @@ package com.swarauto;
 
 public class PcSettings extends Settings {
     public static final String TUTORIALS_DIR_NAME = "tutorial";
-    public static final String PC_VERSION = "1.0.0";
+    public static final String PC_VERSION = "1.0.1";
 
     @Override
     public String getHomeFolderPath() {
